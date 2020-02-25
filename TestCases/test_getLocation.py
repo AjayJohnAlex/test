d42e9bc91d8cca206e2c9b6e6b4d7a5a7a31f0a5
AjayJohnAlex <ajay.alex96@gmail.com>
2020-02-25 15:59:24 +0530

    implementing the earthdistance by mathematical computation

--- TestCases/test_getLocation.py
+++ TestCases/test_getLocation.py
@@ -4,9 +4,11 @@ from .app import app
 
 client = TestClient(app)
 
+# Get api - /get_location : Given lat,lng ... it will fetch pin code, address, city as a json response.
+
 
 def test_getlocation():
-    response = client.get("/post_location/28.6333/77.2167")
+    response = client.get("/get_location/28.6333/77.2167")
     assert response.status_code == 200
     assert response.json() == {
         "Details:": [
@@ -17,6 +19,8 @@ def test_getlocation():
             ]
         ]
     }
+
+# Post api - /post_location : Post lat,lng of any location with pin code+address+city. This api will add new pin code in db.
 
 
 def test_postlocation():
@@ -31,17 +35,16 @@ def test_postlocation():
     assert response.status_code == 200
     assert response.json() == "Query Executed"
 
+# Get api - / get_using_postgres using postgres "earthdistance" to compute all points in 5km radius
+
 
 def test_getUsingPostgres():
-    response = client.get("/post_location/25.5/94.1333")
+    response = client.get("/get_using_postgres/25.5/94.1333")
     assert response.status_code == 200
     assert response.json() == {
         "Pincodes within the radius of 5 KM:": [
             ["IN/795150"],
-            ["IN/795150"],
             ["IN/795151"],
-            ["IN/795151"],
-            ["IN/795158"],
             ["IN/795158"]
         ]
     }
--- app.py
+++ app.py
@@ -28,6 +28,8 @@ class Item(BaseModel):
 async def root():
     return "Hello , This is a Test project."
 
+# Get api - /get_location : Given lat,lng ... it will fetch pin code, address, city as a json response.
+
 
 @app.get('/get_location/{latitude_v}/{longitude_v}')
 async def get_location(latitude_v: float, longitude_v: float):
@@ -47,6 +49,8 @@ async def get_location(latitude_v: float, longitude_v: float):
     except psycopg2.DatabaseError as error:
         print(error)
 
+# Post api - /post_location : Post lat,lng of any location with pin code+address+city. This api will add new pin code in db.
+
 
 @app.post('/post_location/')
 async def post_location(item: Item):
@@ -61,17 +65,19 @@ async def post_location(item: Item):
     cur.close()
     return "Query Executed"
 
+# Get api - / get_using_postgres : using postgres "earthdistance" to compute all points in 5km radius
+
 
 @app.get('/get_using_postgres/{latitude_v}/{longitude_v}')
 async def get_using_postgres(latitude_v: float, longitude_v: float):
 
     cur = con.cursor()
 
-    query = '''select key from location a,
+    query = '''select distinct(key) from location a,
                 lateral (
-                  select place_name,latitude, longitude from location where latitude = (%s) and longitude = (%s)
+                  select place_name,latitude, longitude from location where latitude = 25.5 and longitude = 94.1333
                         ) as hr_jax
-                where a.place_name <> hr_jax.place_name and
+                where a.place_name = hr_jax.place_name and
                 earth_distance(
                         ll_to_earth(a.latitude, a.longitude),
                         ll_to_earth(hr_jax.latitude, hr_jax.longitude)
@@ -86,5 +92,35 @@ async def get_using_postgres(latitude_v: float, longitude_v: float):
     return {"Pincodes within the radius of 5 KM:": data}
 
 
+@app.get('/get_using_self/{latitude_v}/{longitude_v}')
+async def get_using_self(latitude_v: float, longitude_v: float):
+
+    cur = con.cursor()
+    query = '''
+    select key from (
+    select z.key,
+        p.distance_unit * DEGREES(ACOS(GREATEST(1.0, COS(RADIANS(p.latpoint)) * COS(RADIANS(z.latitude))
+         * COS(RADIANS(p.longpoint - z.longitude)) + SIN(RADIANS(p.latpoint)) * SIN(RADIANS(z.latitude))))) AS distance
+    from location AS z
+    join (   /* these are the query parameters */
+        select  25.5  AS latpoint,  94.1333 AS longpoint,
+                5 AS radius,      111.045 AS distance_unit
+        ) AS p ON 1=1
+    where z.latitude
+        between p.latpoint  - (p.radius / p.distance_unit)
+            and p.latpoint  + (p.radius / p.distance_unit)
+    and z.longitude
+        between p.longpoint - (p.radius / (p.distance_unit * COS(RADIANS(p.latpoint))))
+            and p.longpoint + (p.radius / (p.distance_unit * COS(RADIANS(p.latpoint))))
+) as s
+    where distance <= 5
+    order by distance
+ '''
+    input_data = (latitude_v, longitude_v)
+    cur.execute(query, input_data)
+    data = cur.fetchall()
+    cur.close()
+
+    return {"Pincodes within the radius of 5 KM:": data}
 if __name__ == "main":
     uvicorn.run(app, host="127.0.0.1")
